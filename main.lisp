ac9dc49b850125c0f355a50e93ead5ac50f3e286
dunderscore <carsone2017@gmail.com>
2025-06-10 23:56:58 -0400

    fix local variable shadow issue

--- main.lisp
+++ main.lisp
@@ -172,24 +172,18 @@ The GPIO chip returned must be closed with gpio-chip-close"
 (defcfun "gpiod_request_config_set_consumer" :string
   (config (:pointer (:struct gpiod-request-config))))
 
-(defmacro with-pins (pins (&key (path "/dev/gpiochip0") (direction :output) 
-                                (output-value :active)) &body body)
+(defmacro with-pins (pins (&key (path "/dev/gpiochip0") (direction :output)) &body body)
   (if (string= (symbol-name direction) "OUTPUT")
       (setf direction gpiod-line-direction-output)
       (setf direction gpiod-line-direction-input))
-  (if (string= (symbol-name output-value) "ACTIVE")
-      (setf output-value gpiod-line-value-active)
-      (setf output-value gpiod-line-value-inactive))
   `(progn
      (let ((chip (gpiod-chip-open ,path))
            (settings (gpiod-line-settings-new))
            (line-cfg (gpiod-line-config-new))
            (offset (cffi:foreign-alloc :uint :count (length ,pins) :initial-contents ,pins))
-           (:active gpiod-line-value-active)
-           (:inactive gpiod-line-value-inactive)
            (request ()))
        (gpiod-line-settings-set-direction settings ,direction)
-       (gpiod-line-settings-set-output-value settings ,output-value)
+       (gpiod-line-settings-set-output-value settings gpiod-line-value-inactive)
        (gpiod-line-config-add-line-settings line-cfg offset (length offset) settings)
        (setf request (gpiod-chip-request-lines chip (cffi:null-pointer) line-cfg))
        ,@body
@@ -213,13 +207,5 @@ Valid OUTPUT-VALUE values are
         (t (setf output-value 'gpiod-line-value-inactive)))
   `(gpiod-line-request-set-value request ,pin ,output-value))
 
-(with-pins '(5 26) nil
-  (set-pin 26 :high)
-  (sleep 3)
-  (set-pin 26 :low)
-  (sleep 7)
-  (set-pin 5 :active)
-  (sleep 3)
-  (set-pin 5 :inactive))
 
 
